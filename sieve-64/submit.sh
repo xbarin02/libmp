fab134b32715cb62d0f5bcd5c18b9c1992ef76e6
David Barina <ibarina@fit.vutbr.cz>
2016-10-28 01:13:45 +0200

    sieve-64: added SGE script

---
+++ sieve-64/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#$ -N sieve-64
+#$ -o $JOB_NAME.$JOB_ID.out
+#$ -e $JOB_NAME.$JOB_ID.err
+
+# one hour
+$HOME/sieve/sieve-64/sieve-64 -t 3600
+
+# submit again
+qsub -q all.q@@stable -l ram_free=512M $HOME/sieve/sieve-64/submit.sh
