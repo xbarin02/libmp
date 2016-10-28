72dde35bd72ae890db70ce9f27d45b52b4bfac48
David Barina <ibarina@fit.vutbr.cz>
2016-10-28 02:23:11 +0200

    sieve-64: fixed SGE script

--- sieve-64/submit.sh
+++ sieve-64/submit.sh
@@ -4,8 +4,11 @@
 #$ -o sieve/sieve-64/logs/$JOB_NAME.$JOB_ID.out
 #$ -e sieve/sieve-64/logs/$JOB_NAME.$JOB_ID.err
 
-# one hour
-$HOME/sieve/sieve-64/sieve-64 -t 3600
+# change working directory to store a state/record to the right place
+cd $HOME/sieve/sieve-64/
+
+# limit job to one hour
+./sieve-64 -t 3600
 
 # submit again
 qsub -q all.q@@stable -l ram_free=512M $HOME/sieve/sieve-64/submit.sh
