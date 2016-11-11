aee4383cc92b10f269df43c7288188791ebec513
David Barina <ibarina@fit.vutbr.cz>
2016-11-12 00:54:14 +0100

    dlog2: added baby-step giant-step algorithm

--- dlog2/dlog.c
+++ dlog2/dlog.c
@@ -94,6 +94,62 @@ int64_t dlog2_r_lsb(int64_t p, int64_t K)
 	return m;
 }
 
+int64_t int64_ceil_sqrt(int64_t n)
+{
+	assert( n > INT64_0 );
+
+	int64_t x = n;
+	int64_t y = INT64_0;
+
+	while(x != y)
+	{
+		y = x;
+		x = (x + (n + x - INT64_1)/x + INT64_1) >> 1;
+	}
+
+	return x;
+}
+
+// https://en.wikipedia.org/wiki/Baby-step_giant-step
+int64_t dlog2_bga(int64_t p)
+{
+	int64_t m = int64_ceil_sqrt(p);
+
+	int64_t tab[m];
+
+	int64_t aj = INT64_1;
+	for(int64_t j = INT64_0; j < m; j++)
+	{
+		tab[j] = aj;
+		aj <<= 1;
+		if( aj >= p )
+			aj -= p;
+	}
+
+	int64_t am = dlog2_r_lsb(p, m);
+
+	int64_t y = INT64_1;
+	for(int64_t i = INT64_0; i < m; i++)
+	{
+		// because 2^0 == 1
+		if( i > INT64_0 )
+		{
+			for(int64_t j = INT64_0; j < m; j++)
+			{
+				if( y == tab[j] )
+				{
+					return i*m + j;
+				}
+			}
+		}
+		y *= am;
+		y %= p;
+	}
+
+	assert( "failure" );
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	switch(argc)
@@ -106,6 +162,7 @@ int main(int argc, char *argv[])
 			int64_t f = atol(argv[1]);
 			printf("dlog2(%" PRId64 ") = %" PRId64 " (LSB)\n", f, dlog2_lsb(f));
 			printf("dlog2(%" PRId64 ") = %" PRId64 " (MSB)\n", f, dlog2_msb(f));
+			printf("dlog2(%" PRId64 ") = %" PRId64 " (BGA)\n", f, dlog2_bga(f));
 		}
 			break;
 		case 3:
