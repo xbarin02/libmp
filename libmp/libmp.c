48142e4cc96beb302b2e7cc454e02001f82de9de
David Barina <ibarina@fit.vutbr.cz>
2016-11-15 00:38:01 +0100

    libmp: added limited dlog2 128 and (+)

--- libmp/libmp.c
+++ libmp/libmp.c
@@ -215,6 +215,12 @@ int64_t int64_min(int64_t a, int64_t b)
 	return a < b ? a : b;
 }
 
+static
+int128_t int128_min(int128_t a, int128_t b)
+{
+	return a < b ? a : b;
+}
+
 // x in [0; L) : 2^x = 1 (mod p) [LSB, faster]
 static
 int64_t int64_dlog2_mn_lim(int64_t p, int64_t L)
@@ -243,3 +249,90 @@ int64_t int64_dlog2_mn_lim(int64_t p, int64_t L)
 }
 
 int64_t mp_int64_dlog2_mn_lim(int64_t p, int64_t L) { return int64_dlog2_mn_lim(p, L); }
+
+// x in [0; L) : 2^x = 1 (mod p) [LSB, faster]
+static
+int128_t int128_dlog2_mn_lim(int128_t p, int128_t L)
+{
+	assert( p > INT128_0 );
+	assert( p & INT128_1 );
+
+	int128_t K = int128_min(p, L) - INT128_1;
+
+	int128_t m = INT128_1;
+	int128_t k = INT128_0;
+
+	while( k < K )
+	{
+		if( m & INT128_1 )
+			m += p;
+		m >>= 1;
+
+		k++;
+
+		if( INT128_1 == m )
+			return k;
+	}
+
+	return INT128_0;
+}
+
+int128_t mp_int128_dlog2_mn_lim(int128_t p, int128_t L) { return int128_dlog2_mn_lim(p, L); }
+
+// x in [0; L) : 2^x = 1 (mod p) [MSB, slower]
+static
+int64_t int64_dlog2_pl_lim(int64_t p, int64_t L)
+{
+	assert( p > INT64_0 );
+	assert( p & INT64_1 );
+
+	int64_t K = int64_min(p, L) - INT64_1;
+
+	int64_t m = INT64_1;
+	int64_t k = INT64_0;
+
+	while( k < K )
+	{
+		m <<= 1;
+		if( m >= p )
+			m -= p;
+
+		k++;
+
+		if( INT64_1 == m )
+			return k;
+	}
+
+	return INT64_0;
+}
+
+int64_t mp_int64_dlog2_pl_lim(int64_t p, int64_t L) { return int64_dlog2_pl_lim(p, L); }
+
+// x in [0; L) : 2^x = 1 (mod p) [MSB, slower]
+static
+int128_t int128_dlog2_pl_lim(int128_t p, int128_t L)
+{
+	assert( p > INT128_0 );
+	assert( p & INT128_1 );
+
+	int128_t K = int128_min(p, L) - INT128_1;
+
+	int128_t m = INT128_1;
+	int128_t k = INT128_0;
+
+	while( k < K )
+	{
+		m <<= 1;
+		if( m >= p )
+			m -= p;
+
+		k++;
+
+		if( INT128_1 == m )
+			return k;
+	}
+
+	return INT128_0;
+}
+
+int128_t mp_int128_dlog2_pl_lim(int128_t p, int128_t L) { return int128_dlog2_pl_lim(p, L); }
