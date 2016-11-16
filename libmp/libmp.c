6dbc773f8c3cfc2be611e39c15adfc1d903fe6db
David Barina <ibarina@fit.vutbr.cz>
2016-11-16 22:56:33 +0100

    libmp: +TODO comment

--- libmp/libmp.c
+++ libmp/libmp.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+// TODO: implement using exponentiation by squaring (x**=2, x*=b)
 // 2^(+k) (mod p) [MSB, slower]
 static
 int64_t int64_dpow2_pl(int64_t p, int64_t K)
@@ -33,6 +34,7 @@ int64_t int64_dpow2_pl(int64_t p, int64_t K)
 
 int64_t mp_int64_dpow2_pl(int64_t p, int64_t K) { return int64_dpow2_pl(p, K); }
 
+// TODO: implement using exponentiation by squaring (x**=2, x*=b)
 // 2^(+k) (mod p) [MSB, slower]
 static
 int128_t int128_dpow2_pl(int128_t p, int128_t K)
@@ -56,6 +58,7 @@ int128_t int128_dpow2_pl(int128_t p, int128_t K)
 
 int128_t mp_int128_dpow2_pl(int128_t p, int128_t K) { return int128_dpow2_pl(p, K); }
 
+// TODO: implement using exponentiation by squaring (x**=2, x*=b)
 // 2^(-k) (mod p) [LSB, faster]
 static
 int64_t int64_dpow2_mn(int64_t p, int64_t K)
@@ -79,6 +82,7 @@ int64_t int64_dpow2_mn(int64_t p, int64_t K)
 
 int64_t mp_int64_dpow2_mn(int64_t p, int64_t K) { return int64_dpow2_mn(p, K); }
 
+// TODO: implement using exponentiation by squaring (x**=2, x*=b)
 // 2^(-k) (mod p) [LSB, faster]
 static
 int128_t int128_dpow2_mn(int128_t p, int128_t K)
