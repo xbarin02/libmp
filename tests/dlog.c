ec4f8f73c5a74aa7a234323cf0914008bcc613aa
David Barina <ibarina@fit.vutbr.cz>
2016-11-16 22:12:58 +0100

    added tests/

---
+++ tests/dlog.c
@@ -0,0 +1,73 @@
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <assert.h>
+#include <libmp.h>
+
+// naive implementation as the reference one
+static
+int64_t dlog2(int64_t p)
+{
+	assert( p > INT64_0 );
+
+	int64_t m = INT64_1;
+	int64_t k = INT64_0;
+
+	while( k < p - INT64_1 )
+	{
+		m <<= 1;
+		if( m >= p )
+			m -= p;
+
+		k++;
+
+		if( INT64_1 == m )
+			return k;
+		if( INT64_0 == m )
+			return INT64_0;
+	}
+
+	return INT64_0;
+}
+
+int main()
+{
+	// for each range
+	for(int bit_level = 0; bit_level < 64; bit_level++)
+	{
+		printf("testing the bit level %i...\n", bit_level);
+
+		// for each ODD factor in [ 2^bit_level .. 2^(bit_level+1)-1 )
+		for(int64_t f = (INT64_1<<bit_level) + 1; f < (INT64_1<<(bit_level+1)); f += 2)
+		{
+			// reference
+			int64_t r = dlog2(f);
+
+			// 64-bit tests
+			// TEST: mp_int64_dlog2_mn
+			assert( r == mp_int64_dlog2_mn(f) );
+			// TEST: mp_int64_dlog2_pl
+			assert( r == mp_int64_dlog2_pl(f) );
+			// TEST: mp_int64_dlog2_mn_lim
+			assert( r == mp_int64_dlog2_mn_lim(f, INT64_1<<62) );
+			// TEST: mp_int64_dlog2_pl_lim
+			assert( r == mp_int64_dlog2_pl_lim(f, INT64_1<<62) );
+			// TEST: mp_int64_dlog2_bg_lim
+			assert( r == mp_int64_dlog2_bg_lim(f, INT64_1<<62) );
+
+			// 128-bit tests
+			// TEST: mp_int128_dlog2_mn
+			assert( r == mp_int128_dlog2_mn(f) );
+			// TEST: mp_int128_dlog2_pl
+			assert( r == mp_int128_dlog2_pl(f) );
+			// TEST: mp_int128_dlog2_mn_lim
+			assert( r == mp_int128_dlog2_mn_lim(f, INT128_1<<126) );
+			// TEST: mp_int128_dlog2_pl_lim
+			assert( r == mp_int128_dlog2_pl_lim(f, INT128_1<<126) );
+			// TEST: mp_int128_dlog2_bg_lim
+			assert( r == mp_int128_dlog2_bg_lim(f, INT128_1<<126) );
+		}
+	}
+
+	return 0;
+}
